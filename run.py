f5044e83fe3c552ec0544af6db828b5063c22ce7
frankiesanjana <francesca.sanjana@gmail.com>
2022-06-27 14:07:55 +0000

    Add mortgage class and example mortgage instances

--- run.py
+++ run.py
@@ -6,4 +6,26 @@ import pandas as pd
 import numpy as np
 from datetime import date
 
-print("test")
+class MortgageCalculator:
+    """
+       Defines the main class for mortgage calculation
+    """
+    def __init__(self, interest, years, mortgage):
+        self.interest = interest
+        self.years = years
+        self.mortgage = mortgage
+
+    def definition(self):
+        """
+        Describes the mortgage
+        """
+        return f"A {self.years}-year mortgage for a total amount borrowed of ${self.mortgage} at an interest rate of {self.interest}%"
+
+MortgageA = MortgageCalculator(4.0, 20, 250000)
+print(MortgageA.definition())
+
+MortgageB = MortgageCalculator(4.0, 30, 250000)
+print(MortgageB.definition())
+
+MortgageC = MortgageCalculator(4.5, 30, 250000)
+print(MortgageC.definition())
