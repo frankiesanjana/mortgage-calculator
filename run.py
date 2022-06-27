00011b34ad9276c3cead1ca776430530f4ef3153
frankiesanjana <francesca.sanjana@gmail.com>
2022-06-27 11:24:34 +0000

    Install and import external libraries

--- run.py
+++ run.py
@@ -2,4 +2,8 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+import pandas as pd
+import numpy as np
+from datetime import date
+
 print("test")
