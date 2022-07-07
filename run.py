21c397e8bc72cfd3f17ec818eb4e62a408b8d31d
frankiesanjana <francesca.sanjana@gmail.com>
2022-07-07 11:27:54 +0000

    Add details to choose_example function

--- run.py
+++ run.py
@@ -7,61 +7,6 @@ import numpy as np
 import numpy_financial as npf
 from datetime import date
 
-
-def choose_example():
-    while True:
-        print("You have chosen to view an example mortgage.")
-        print("Please select from the following options:")
-        print("Type 'a' to view details of a 20-year mortgage for a total amount borrowed of $250,000 with an interest rate of 4.0%")
-        print("Type 'b' to view details of a 30-year mortgage for a total amount borrowed of $250,000 with an interest rate of 4.0%")
-        print("Type 'c' to view details of a 30-year mortgage for a total amount borrowed of $250,000 with an interest rate of 4.5%")
-        print("Type 'd' to view details of all of the above mortgages at once.")
-        print("Type 'x' to exit and return to the main menu.")
-        
-        choice = input("Enter your selection here:\n")
-
-        if choice == "a":
-            print("xxx")
-            break
-        elif choice == "b":
-            print("yyy")
-            break
-        elif choice == "c":
-            print("zzz")
-            break
-        elif choice == "d":
-            print("aaa")
-            break
-        elif choice == "x":
-            print("bbb")
-            break
-        else:
-            print("Invalid input, please try again.\n")
-
-
-def welcome_user():
-    """
-       Introductory screen for the user
-    """
-    while True:
-        print("Welcome to the mortgage calculator!")
-        print("Please select from the following options:")
-        print("Type 'a' to view examples of different mortgages, or 'b' to input your own details.")
-
-        choice = input("Enter your selection here:\n")
-
-        if choice == "a":
-            choose_example()
-            break
-        elif choice == "b":
-            print("yyy")
-            break
-        else:
-            print("Invalid input, please try again.\n")
-
-welcome_user()
-
-
 class MortgageCalculator:
     """
        Defines the main class for mortgage calculation
@@ -80,13 +25,74 @@ class MortgageCalculator:
         """
         return f"A {self.years}-year mortgage for a total amount borrowed of ${self.mortgage} at an interest rate of {self.interest}%\nhas a monthly payment amount of ${self.monthly_payment}.\nThe total amount repaid on this mortgage by the end of the term will be ${self.total_repayment}.\n"
 
+
+"""
+    Defines some example mortgages to display to the user
 """
 MortgageA = MortgageCalculator(4.0, 20, 250000)
-print(MortgageA.definition())
-
 MortgageB = MortgageCalculator(4.0, 30, 250000)
-print(MortgageB.definition())
-
 MortgageC = MortgageCalculator(4.5, 30, 250000)
-print(MortgageC.definition())
-"""
+
+def choose_example():
+    """
+       Allows user to select an example mortgage and view details
+    """
+    while True:
+        print("You have chosen to view example mortgages.")
+        print("Please select from the following options:")
+        print("Type 'a' to view details of a 20-year mortgage for a total amount borrowed of $250,000 with an interest rate of 4.0%")
+        print("Type 'b' to view details of a 30-year mortgage for a total amount borrowed of $250,000 with an interest rate of 4.0%")
+        print("Type 'c' to view details of a 30-year mortgage for a total amount borrowed of $250,000 with an interest rate of 4.5%")
+        print("Type 'd' to view details of all of the above mortgages at once.")
+        print("Type 'x' to return to the main menu.")
+        print("Type 'z' to exit the mortgage calculator.")
+        
+        choice = input("Enter your selection here:\n")
+
+        if choice == "a":
+            print(MortgageA.definition())
+        elif choice == "b":
+            print(MortgageB.definition())
+        elif choice == "c":
+            print(MortgageC.definition())
+        elif choice == "d":
+            print(MortgageA.definition())
+            print(MortgageB.definition())
+            print(MortgageC.definition())
+        elif choice == "x":
+            welcome_user()
+            break
+        elif choice == "z":
+            print("Thank you for using the mortgage calculator and goodbye.")
+            break
+        else:
+            print("Invalid input, please try again.\n")
+
+def welcome_user():
+    """
+       Introductory screen for the user
+    """
+    while True:
+        print("Welcome to the mortgage calculator!")
+        print("Please select from the following options:")
+        print("Type 'a' to view examples of different mortgages.")
+        print ("Type 'b' to input your own details.")
+        print("Type 'z' to exit the mortgage calculator.")
+
+        choice = input("Enter your selection here:\n")
+
+        if choice == "a":
+            choose_example()
+            break
+        elif choice == "b":
+            print("yyy")
+            break
+        elif choice == "z":
+            print("Thank you for using the mortgage calculator and goodbye.")
+            break
+        else:
+            print("Invalid input, please try again.\n")
+
+welcome_user()
+
+
